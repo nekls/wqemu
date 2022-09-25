fc40e574fcff4d5f419c10f9e164bfbfeb238524
nekls <37667068+nekls@users.noreply.github.com>
2022-09-25 13:24:24 +0200

    Update w10x64gitpod.sh

--- w10x64gitpod.sh
+++ w10x64gitpod.sh
@@ -30,12 +30,3 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "===================================="
 echo "Username: akuh"
 echo "Password: Akuh.Net"
-echo "===================================="
-echo "===================================="
-echo "Keep supporting akuh.net, thank you"
-echo "You Got Free RDP now"
-echo "Wait 2 minute to finish bot"
-echo "You can close this tab"
-echo "RDP runs for 50 hours"
-echo "===================================="
-sleep 432000
